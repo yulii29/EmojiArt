16b8bcb664dada060549c81632bf40fc080375ee
Iiuliia <yulixan29@gmail.com>
2024-04-25 14:40:47 +0300

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Юлия Прищепкина on 25.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
